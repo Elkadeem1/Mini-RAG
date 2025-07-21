266061ececf38473d4cb9b2b37a00189f091f070
Old <mahmoudelkadeem32@gmail.com>
2025-07-21 15:50:12 +0300

    refactor: rename the class

--- src/routes/schemes/data.py
+++ src/routes/schemes/data.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 
-class proccesRequest(BaseModel):
-   field_id : str
+class ProcessRequest(BaseModel):
+   file_id : str
    chunk_size: Optional[int] = 100
    overlap_size: Optional[int] = 20
    do_reset: Optional[int] = 0
