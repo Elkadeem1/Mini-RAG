88bac212f62570158e0bcd0aa042a75aadaad1da
Old <mahmoudelkadeem32@gmail.com>
2025-07-27 17:09:34 +0300

    feat(models): add get_indexes classmethod for chunk_project_id

--- src/models/db_schemes/data_chunk.py
+++ src/models/db_schemes/data_chunk.py
@@ -3,11 +3,25 @@ from typing import Optional
 from bson import ObjectId
 
 class DataChunk(BaseModel):
-    id: Optional[ObjectId]
-    chunk_text: str = Field(...,min_length=1)
+    id: Optional[ObjectId] = Field(None, alias="_id")
+    chunk_text: str = Field(..., min_length=1)
     chunk_metadata: dict
-    chunk_order : str = Field(..., gt=0)
+    chunk_order: int = Field(..., gt=0)
     chunk_project_id: ObjectId
+    chunk_asset_id: ObjectId
 
     class Config:
         arbitrary_types_allowed = True
+
+
+    @classmethod
+    def get_indexes(cls):
+        return [
+            {
+                "key": [
+                    ("chunk_project_id", 1)
+                    ],
+                "name": "chunk_project_id_index_1",
+                "unique": False
+            }
+        ]
